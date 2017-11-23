5fc18e8a6552cd1b7ed291a40ea2de365f09a164
danielkorbel2 <danielkorbel2@danielkorbel2-H87-HD3>
2017-11-23 22:08:31 +0100

    Initial commit

---
+++ ui-gwtMaterialDesignAdapter/src/main/java/pl/korbeldaniel/ui/ErpButton.java
@@ -0,0 +1,10 @@
+package pl.korbeldaniel.ui;
+
+import gwt.material.design.client.ui.MaterialButton;
+
+public class ErpButton extends MaterialButton implements IsButton {
+	public ErpButton(String text) {
+		super(text);
+	}
+	
+}
---
+++ ui-gwtMaterialDesignAdapter/src/main/java/pl/korbeldaniel/ui/ErpDiv.java
@@ -0,0 +1,9 @@
+package pl.korbeldaniel.ui;
+
+public class ErpDiv implements IsDiv {
+
+	public IsDiv getElement() {
+		return this;
+	}
+
+}
