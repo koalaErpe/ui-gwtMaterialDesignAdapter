d7361f6a0c7d067fb19c04cf4c97aa5e3be83124
danielkorbel2 <danielkorbel2@danielkorbel2-H87-HD3>
2017-12-09 22:10:42 +0100

    fixed rest api issue

--- src/main/java/pl/korbeldaniel/ui/client/ErpButton.java
+++ src/main/java/pl/korbeldaniel/ui/client/ErpButton.java
@@ -3,14 +3,15 @@ package pl.korbeldaniel.ui.client;
 import com.google.gwt.user.client.ui.Widget;
 
 import gwt.material.design.client.ui.MaterialButton;
-import pl.korbeldaniel.ui.client.IsButton;
+//import pl.korbeldaniel.ui.client.IsButton;
 
-public class ErpButton  implements IsButton {
+public class ErpButton { //implements IsButton {
+//public class ErpButton  implements IsButton {
 	public MaterialButton button;
 	public ErpButton(String text) {
 		this.button = new MaterialButton(text);
 	}
-	@Override
+	//@Override
 	public Widget asWidget() {
 		return button.asWidget();
 	}
